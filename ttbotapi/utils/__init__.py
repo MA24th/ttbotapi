d18aea344010cf0c8dd57a8b4063584e36199100
Mustafa Asaad <ma24th@yahoo.com>
2022-03-21 01:05:47 +0300

    Add utils package

---
+++ ttbotapi/utils/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+"""
+ttbotapi
+~~~~~~~~
+:copyright: (c) 2022 by Mustafa Asaad.
+:license: GPLv2, see LICENSE for more details.
+"""
+
+from .api_exceptions import *
+from .api_handler import *
+from .json_helper import *
+from .logger import *
