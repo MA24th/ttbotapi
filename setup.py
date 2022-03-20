2b30dde8826d5530e0ef30eddd788d69e08055ba
Mustafa Asaad <ma24th@yahoo.com>
2022-03-21 01:03:25 +0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+
+def read(filename):
+    with open(filename, encoding='utf-8') as file:
+        return file.read()
+
+setup(
+    name='ttbotapi',
+    version='0.3.0',
+    description='TamTam Bot API library',
+    long_description=read("README.md"),
+    author='Mustafa Asaad',
+    author_email='ma24th@yahoo.com',
+    url='https://github.com/MA24th/tgbotapi',
+    packages=find_packages(),
+    classifiers=['Development Status :: 5 - Production/Stable',
+                 'Programming Language :: Python :: 3.6',
+                 'Programming Language :: Python :: 3.7',
+                 'Programming Language :: Python :: 3.8',
+                 'Programming Language :: Python :: 3.9',
+                 'License :: OSI Approved :: GNU General Public License v2 (GPLv2)'],
+    license='GNU GPLv2'
+)
